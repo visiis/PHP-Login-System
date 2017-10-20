7caf0fda172c0f7d2cda7626ecf0ef6f087c2ad0
曾玟凱 <zll12545@gmail.com>
2017-10-21 01:58:39 +0800

    Added dashboard

--- dashboard.php
+++ dashboard.php
@@ -4,8 +4,10 @@
 	define('__CONFIG__', true);
 	//Require the config
 	require_once "inc/config.php";
-	echo $_SESSION['user_id'];
-	exit;
+	
+
+	ForceLogin();
+
 ?>
 
 <!DOCTYPE html>
@@ -15,15 +17,9 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="robots" content="follow">
-  	<base href="" target="_blank"/>
 
 	<title>Document</title>
 
-	<style>
-		body {
-			font-family: "Microsoft YaHei","Heiti TC";
-		}
-	</style>
 	<!-- UIkit CSS -->
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.30/css/uikit.min.css" />
 </head>
@@ -31,13 +27,7 @@
 
 
 	<div class="uk-section uk-container ul-text-center">
-		<?php
-			echo 'Today is :' . date('Y m d');
-		?>
-		<p>
-			<a href="login.php">登入</a>
-			<a href="register.php">註冊</a>
-		</p>
+		Dashboard here
 	</div>
 
 
